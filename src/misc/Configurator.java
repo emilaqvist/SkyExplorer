1b285b6fb911b05e15e8bc987c20df3ba7217d89
emilaqvist <emilaqvist@gmail.com>
2025-03-05 15:46:12 +0100

    Fix med Väder klasser
    Lyckades få in API att fungera med väder klasser, dock med manuell input.

---
+++ src/misc/Configurator.java
@@ -0,0 +1,25 @@
+package misc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configurator {
+    private static final Properties properties = new Properties();
+
+    static{
+        try(InputStream inputStream = Configurator.class.getClassLoader().getResourceAsStream("application.properties")){
+            if (inputStream == null){
+                System.err.println("application.properties filen kunde ej hittas!");
+            }else {
+                properties.load(inputStream);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
--- src/service/WeatherService.java
+++ src/service/WeatherService.java
@@ -1,4 +1,62 @@
 package service;
 
+
+import misc.Configurator;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class WeatherService {
+
+    private static final String API_NYCKEL = Configurator.getProperty("TOMOROW_API_KEY");
+    private static final String URL_BASE = "https://api.tomorrow.io/v4/weather/forecast";
+    public String getWeatherData(String location) {
+
+        try{
+            if (API_NYCKEL == null || API_NYCKEL.isEmpty()){
+                return "{\"error\": \"API-nyckel saknas. Kontrollera att den är satt i application.properties.\"}";
+            }
+
+            String encodedLocation = URLEncoder.encode(location.trim(), StandardCharsets.UTF_8.toString());
+            String encodeFields = URLEncoder.encode("temperature,nederbordTyp",StandardCharsets.UTF_8.toString());
+
+            String timeStepsValue = "1h";
+            String urlString = URL_BASE + "?location=" + encodedLocation
+                    + "&fields=" + encodeFields
+                    + "&timesteps=" + timeStepsValue
+                    + "&units=metric"
+                    + "&apikey=" + API_NYCKEL;
+
+            System.out.println("Färdigställd URL: " + urlString);
+
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if(responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String inputLine;
+                while ((inputLine = bufferedReader.readLine()) != null){
+                    response.append(inputLine);
+                }
+                bufferedReader.close();
+                return response.toString();
+            }else {
+                return "{\"error\":\"GET request failade och returnerade http status: " + responseCode + "\"}";
+            }
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return "{\"error\": \"Exception i VäderDatan: " + e.getMessage() + "\"}";
+        }
+
+    }
 }
