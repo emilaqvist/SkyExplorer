53510a8b9de2c5b3ff931ae6bd09e6589bdaed2a
AmerSab <amer-sab@hotmail.com>
2025-06-07 00:51:43 +0200

    Test1 av att lägga till attractions i uppdaterad version
    Saknar möjligtvis knapp för att hitta info om stad "x"

---
+++ src/main/java/dto/CityInfo.java
@@ -0,0 +1,32 @@
+package dto;
+
+/**
+ * Representerar en attraction med detaljerad information via alla parametrar
+ * @author Amer Sabaredzovic
+ */
+public class CityInfo {
+    private String title;  // Namnet på sevärdheten
+    private String description;  // Kort beskrivning
+    private String url;  // Länk till Wikipedia-artikeln
+    private String imageUrl; // Bild-URL (om finns)
+
+    // Konstruktor
+    public CityInfo(String title, String description, String imageUrl, String url) {
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.imageUrl = imageUrl;
+    }
+
+    // Getters och Setters
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public String getUrl() { return url; }
+    public String getImageUrl() { return imageUrl; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+    public void setUrl(String url) { this.url = url; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+
+}
